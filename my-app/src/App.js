b5f9f9613912fab0a1c8524e7220e6f8194f3f35
RushmalP <s3896471@student.rmit.edu.au>
2024-04-11 13:12:17 +1000

    workingLogInLogOut

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './fragments/Navbar';
-import Footer from './fragments/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./fragments/Navbar";
+import Footer from "./fragments/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import Specials from './pages/Specials';
-import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import DietPlan from './pages/DietPlan';
 import Cart from './pages/Cart';
+import { getUser, removeUser } from "./data/userPass";
 
 
 function App() {
+  const [username, setUsername] = useState(getUser());
+
+  const loginUser = (username) => {
+    setUsername(username);
+  }
+
+  const logoutUser = () => {
+    removeUser();
+    setUsername(null);
+  }
+
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Navbar />
-        <main role="main" className="flex-shrink-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/specials" element={<Specials />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/dietplan" element={<DietPlan />} />
-            <Route path="/cart" element={<Cart />} />
-            {/* Additional routes as needed */}
-          </Routes>
+    <div className="d-flex flex-column min-vh-100">
+      <Router>
+        <Navbar username={username} logoutUser={logoutUser} />
+        <main role="main">
+          <div className="container my-3">
+            <Routes>
+              <Route path="/" element={<Home username={username} />} />
+              <Route path="/login" element={<Login loginUser={loginUser} />} />
+              <Route path="/profile" element={<Profile username={username} />} />
+              <Route path="/specials" element={<Specials />} />
+              <Route path="/signIn" element={<SignIn />} />
+              <Route path="/dietplan" element={<DietPlan />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </div>
         </main>
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
