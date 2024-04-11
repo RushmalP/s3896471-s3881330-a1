637ac1eb2088f64dbe70d4a10ddd58e8205471b0
RushmalP <s3896471@student.rmit.edu.au>
2024-04-12 01:02:27 +1000

    logoutmodal

--- my-app/src/fragments/Navbar.js
+++ my-app/src/fragments/Navbar.js
@@ -1,73 +1,91 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import LogoutModal from './LogoutModal'; // Make sure the path is correct
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 
 function Navbar({ username, logoutUser }) {
   const navigate = useNavigate();
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   const handleLogout = () => {
     logoutUser();
-    navigate("/");
+    setShowLogoutModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowLogoutModal(false);
+    navigate('/');
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <div className="logo">
-            <img src="/tree.jpg" alt="SOIL"></img>
-          </div>
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/specials">Specials</Link>
-            </li>
-            {username && (
-              <>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <div className="logo">
+              <img src="/tree.jpg" alt="SOIL" />
+            </div>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/specials">Specials</Link>
+              </li>
+              {username && (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/dietplan">Diet Plan</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/cart">Cart</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/profile">Profile</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+            {username ? (
+              <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/dietplan">Diet Plan</Link>
+                  <span className="navbar-text me-3">
+                    Welcome, {username}
+                  </span>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/cart">Cart</Link>
+                  <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signin">Sign In</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/profile">Profile</Link>
+                  <Link className="nav-link" to="/login">Login</Link>
                 </li>
-              </>
+              </ul>
             )}
-          </ul>
-          {username ? (
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <span className="navbar-text me-3">
-                  Welcome, {username}
-                </span>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button>
-              </li>
-            </ul>
-          ) : (
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/signin">Sign In</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-            </ul>
-          )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <LogoutModal show={showLogoutModal} onClose={handleCloseModal} />
+    </>
   );
 }
 
