440af510d0e3bfc963168ced3fcd1ae458273931
RushmalP <s3896471@student.rmit.edu.au>
2024-04-15 17:09:03 +1000

    Logoutfixed

--- my-app/src/fragments/LogoutModal.js
+++ my-app/src/fragments/LogoutModal.js
@@ -1,20 +1,19 @@
-// LogoutModal.js
 import React from 'react';
 import './LogoutModal.css'; // Make sure the path is correct
 
-function LogoutModal({ show, onClose }) {
-  if (!show) return null;
+ function LogoutModal({ show, onClose }) {
+   if (!show) return null;
 
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <div className="modal-body">
-          <p>You are logged out. Log back in to purchase any Products or to use your custom Diet Plan.</p>
-          <button onClick={onClose}>OK</button>
-        </div>
-      </div>
-    </div>
-  );
-}
+   return (
+     <div className="modal">
+       <div className="modal-content">
+         <div className="modal-body">
+           <p>You are logged out. Log back in to purchase any Products or to use your custom Diet Plan.</p>
+           <button onClick={onClose}>OK</button>
+         </div>
+       </div>
+     </div>
+   );
+ }
 
-export default LogoutModal;
+ export default LogoutModal;
